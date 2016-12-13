f6001e2bbea2da7aa0d6507d51fb5b1d1ed75557
daxnet <daxnet@outlook.com>
2016-12-13 12:57:39 +0800

    Added the basic interfaces for entity and aggregate root.

---
+++ src/Apworks/IAggregateRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apworks
+{
+    /// <summary>
+    /// Represents that the implemented classes are aggregate roots.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the aggregate root key.</typeparam>
+    public interface IAggregateRoot<TKey> : IEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+    }
+}
---
+++ src/Apworks/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apworks
+{
+    /// <summary>
+    /// Represents that the implemented classes are entities.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the entity key.</typeparam>
+    public interface IEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        /// <summary>
+        /// Gets or sets the key of the entity.
+        /// </summary>
+        TKey Id { get; set; }
+    }
+}
