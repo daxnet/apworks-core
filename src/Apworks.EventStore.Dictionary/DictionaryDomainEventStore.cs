620dbea86bb017a583bb24245bd9ac30b0bb6ef6
daxnet <daxnet@outlook.com>
2017-04-20 20:17:45 +0800

    Adding the in-memory dictionary-based domain event store.

---
+++ src/Apworks.EventStore.Dictionary/DictionaryDomainEventStore.cs
@@ -0,0 +1,22 @@
+﻿using Apworks.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Apworks.EventStore.Dictionary
+{
+    public class DictionaryDomainEventStore : DomainEventStore
+    {
+        private readonly ConcurrentDictionary<string, List<IDomainEvent>> bank = new ConcurrentDictionary<string, List<IDomainEvent>>();
+
+        public override IEnumerable<IDomainEvent> Load<TKey>(string aggregateRootTypeIdentifier, TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(IEnumerable<IDomainEvent> events)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
