4fcaba406a548cd4da4b723bd650e2e24eeb03e2
daxnet <daxnet@outlook.com>
2017-06-22 21:35:19 +0800

    Adding the script to start RabbitMQ docker container for the integration testing.

--- build-scripts/init-itest-env.sh
+++ build-scripts/init-itest-env.sh
@@ -11,3 +11,9 @@ docker run -d -p 27017:27017 --name apworks_mongodb mongo
     docker rm $(docker stop $(docker ps -aqf "name=apworks_psql" --format="{{.ID}}")); \
 fi'
 docker run -d -p 5432:5432 -e POSTGRESQL_USER=test -e POSTGRESQL_PASS=oe9jaacZLbR9pN -e POSTGRESQL_DB=test --name apworks_psql orchardup/postgresql
+
+# Setting up Docker container for RabbitMQ
+/bin/bash -c 'if [[ "$(docker ps -aqf "name=apworks_rabbitmq" 2> /dev/null)" != "" ]]; then \
+    docker rm $(docker stop $(docker ps -aqf "name=apworks_rabbitmq" --format="{{.ID}}")); \
+fi'
+sudo docker run -d -p 5672:5672 -p 4369:4369 -p 5671:5671 -p 25672:25672 --name apworks_rabbitmq rabbitmq
