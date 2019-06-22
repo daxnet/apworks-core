05ecf315023b25be6fdaf3ebac427a8e338782da
daxnet <daxnet@outlook.com>
2019-06-22 11:50:33 +0800

    Modifying the registration page of the EasyBooking.IdentityServer.

---
+++ examples/EasyBooking/EasyBooking.IdentityServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EasyBooking.IdentityServer.Data;
+using EasyBooking.IdentityServer.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EasyBooking.IdentityServer.Areas.Identity.IdentityHostingStartup))]
+namespace EasyBooking.IdentityServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
